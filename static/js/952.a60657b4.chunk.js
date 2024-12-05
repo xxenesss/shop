f92f5468cb3ed7fe2f3b1cb4e87b86fd0237f459
vcentrevkusa <quadromapubg@gmail.com>
2024-12-05 20:25:39 +0300

    build

---
+++ static/js/952.a60657b4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_42thug=self.webpackChunk_42thug||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.a60657b4.chunk.js.map
